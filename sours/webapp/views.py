75aece1b560d7a570f75a3fabdc444a595aaaff7
Sagynbek <jyldyzbekov-s@mail.ru>
2023-12-10 17:09:31 +0600

    реализовал представление product_view

--- sours/webapp/views.py
+++ sours/webapp/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from webapp.models import Product
+
 
 def products_view(request):
     products = Product.objects.all()
     return render(request, 'products_list.html', {'products': products})
+
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'products_view.html', {'product': product})
